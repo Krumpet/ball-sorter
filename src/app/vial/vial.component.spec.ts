c42603476adcea6ec38dfd1d0849f3ddddde6b41
Krumpet <ran.lottem@campus.technion.ac.il>
2021-10-18 22:51:02 +0300

    update to ng 12

--- src/app/vial/vial.component.spec.ts
+++ src/app/vial/vial.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { VialComponent } from './vial.component';
 
@@ -6,7 +6,7 @@ describe('VialComponent', () => {
   let component: VialComponent;
   let fixture: ComponentFixture<VialComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ VialComponent ]
     })
